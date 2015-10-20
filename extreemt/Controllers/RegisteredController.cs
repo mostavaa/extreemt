aebcad8b14d52f936c65a0be6552ea12af9621bf
mohameds93 <msalah379@gmail.com>
2015-10-20 16:22:40 +0200

    After Login PAges

---
+++ extreemt/Controllers/RegisteredController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace extreemt.Controllers
+{
+    public class RegisteredController : Controller
+    {
+        //
+        // GET: /Registered/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //After_Login  Cash Commission Dropdown
+        public ActionResult Cash_Bank()
+        {
+
+            return View();
+
+        }
+        public ActionResult Product_Bank()
+        {
+            return View();
+        }
+        public ActionResult Account_Summary()
+        {
+            return View();
+        }
+
+        //After_Login My Team Dropdown
+
+        public ActionResult Group_list()
+        {
+            return View();
+        }
+
+        //After_Login My Signup 
+
+        public ActionResult Daily_Signup()
+        {
+            return View();
+        }
+
+        //After_Login Transfer
+
+        public ActionResult My_Transfer()
+        {
+            return View();
+
+        }
+        //After_Login My Profile
+
+        public ActionResult My_Basket()
+        {
+            return View();
+
+        }
+        public ActionResult My_Order()
+        {
+            return View();
+        }
+
+
+        }
+}
