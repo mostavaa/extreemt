b141f29aff085abab0e73edce44364a1210a614c
mohameds93 <msalah379@gmail.com>
2015-10-26 17:07:56 +0200

    Fix Update2 conflict

--- extreemt/Controllers/HomeController.cs
+++ extreemt/Controllers/HomeController.cs
@@ -16,23 +16,15 @@ namespace extreemt.Controllers
             return View();
         }
 
-<<<<<<< HEAD
-        }
         public ActionResult Category()
         {
             return View();
         }
-      
-        public ActionResult Profile()
+     
+        public ActionResult Update_Category()
         {
             return View();
         }
-        public  ActionResult Update_Category()
-        {
-            return View();
-=======
->>>>>>> 1552016cd5186ef7b43dba1bcf613068a253d068
-
         public ActionResult Profile()
         {
             return View();
