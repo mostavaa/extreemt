5cab2cf3400fabc5a51ac8873f1da50e2371da4d
mohameds93 <msalah379@gmail.com>
2016-01-13 21:39:42 +0200

    paging Group List

--- extreemt/Controllers/RegisteredController.cs
+++ extreemt/Controllers/RegisteredController.cs
@@ -42,16 +42,52 @@ namespace extreemt.Controllers
 
         public ActionResult Group_list()
         {
+
+            int totalRows, page = 1, size, offset, numOfPages;
+
+            if (Request.Params["page"] != null && Request.Params["page"] != "")
+            {
+                page = int.Parse(Request.Params["page"]);
+            }
+
+            page--;
+            size = 3;
+            offset = page * size;
+
+            
             extreemt.user user = extreemt.Account.staticGetLoggedUser();
             extreemt.extreemtEntities db = new extreemt.extreemtEntities();
             List<extreemt.user> allOldUsers = null;
             if (db.users.Where(u => u.registerDate > user.registerDate).Count() > 0)
             {
-                allOldUsers = db.users.Where(u => u.registerDate > user.registerDate).OrderByDescending(o => o.registerDate).ToList();
+                
+                allOldUsers = db.users.Where(u => u.registerDate > user.registerDate).OrderByDescending(o => o.registerDate).Skip(offset).Take(size).ToList();
             }
             Account acc = new Account();
             List<user> allRegisterdChilds = acc.getParentChildren(user, allOldUsers);
             ViewData["allRegisterdChilds"] = allRegisterdChilds;
+
+
+            totalRows = allRegisterdChilds.Count;
+
+
+            //pagination
+            //totalRows 
+            //page = 1;
+
+
+            numOfPages = totalRows / size;
+            if (totalRows % size == 0)
+            {
+                ViewData["numOfPages"] = numOfPages;
+            }
+            else
+            {
+                ViewData["numOfPages"] = numOfPages + 1;
+            }
+
+            
+            
             return View();
         }
 
